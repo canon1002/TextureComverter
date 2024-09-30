8dedce04bedff6b1c028a4fc7cdfe82f6d9b4062
canon1002 <k022g0038@g.neec.ac.jp>
2024-09-30 14:36:41 +0900

    Hello World! 実装

--- main.cpp
+++ main.cpp
@@ -3,14 +3,7 @@
 
 int main(int argc,char* argv[]) {
 
-	// argcの回数分ループする
-	for (int i = 0; i < argc; i++) {
-		// 文字列argvのi番目を表示
-		printf(argv[i]);
-		// 改行
-		printf("\n");
-	}
-	
+	printf("Hello World!\n");
 	system("pause");
 
 	return 0;
