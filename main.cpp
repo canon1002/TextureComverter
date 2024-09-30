d5ade646e470b7ab403ebc36ebfa85afbaac6b42
canon1002 <k022g0038@g.neec.ac.jp>
2024-09-30 14:32:48 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+#include <cstdlib>
+
+int main(int argc,char* argv[]) {
+
+	// argcの回数分ループする
+	for (int i = 0; i < argc; i++) {
+		// 文字列argvのi番目を表示
+		printf(argv[i]);
+		// 改行
+		printf("\n");
+	}
+	
+	system("pause");
+
+	return 0;
+}
